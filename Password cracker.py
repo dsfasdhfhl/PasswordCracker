01d53e3375ef8510a3dacbb47eb218b03edb54e0
4C1D <64551688+dsfasdhfhl@users.noreply.github.com>
2020-07-08 21:38:19 -0500

    Update Password cracker.py

--- Password cracker.py
+++ Password cracker.py
@@ -1,5 +1,6 @@
 import hashlib
 
+print("\033[1;36;40m\033\n")
 print("""{}
     
     ██╗  ██╗ ██████╗ ██╗██████╗                                                    
@@ -24,11 +25,13 @@ print("""{}
                          ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ 
 {}\n\tWelcome To 4C1D's Password Cracker (The Hashes WON'T Show)\n\tSubscribe To Linux Hacker On YT, And Follow linux_hacker_4c1d On IG\n{}""".format("="*100,"="*100,"="*100))
 
+print("\033[1;37;40m\n")
 
 p_hash = input("Enter MD5 Hash Here --> ")
 
 w_list = input("Enter Wordlist Here --> ")
 
+print("\033[1;34;40m\n")
 print("Please Wait The Password Is Being Cracked This Might Take A Bit...")                
 print("So While You Wait Dance With The Robots └[∵┌]└[ ∵ ]┘[┐∵]┘")
 
@@ -45,17 +48,25 @@ for _word in password_file:
 
 
 	if digs == p_hash:
+		print("\033[1;32;40m\n")
 		print("==================================")
 		print("Password Found! ٩(^ᴗ^)۶")
 		print("The Password Is {}".format(_word))
 		print("Closing Program... Bye (•◡•) /")
+		print("==================================")		
+		print("Come Back Soon (•◡•) /")
 		print("==================================")
 		password_file.close()
 		fg=2
 		break
 if fg == 1:
+	print("\033[1;31;40m\n")
 	print("==================================")	
 	print("Password Not Found (︶︹︺)")
 	print("Closing Program... Bye (•◡•) /")
-	print("==================================")
+	print("==================================")	
+	print("Come Back Soon (•◡•) /")
+	print("==================================")	
+
 	quit()
+
